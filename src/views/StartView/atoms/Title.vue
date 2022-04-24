2a20be559dffe0c88aeac17c632c552b08bc8869
psevdo:Proger <psevdoproger@yandex.ru>
2022-04-24 15:28:05 +0300

    feat: add raw implementation of main page

---
+++ src/views/StartView/atoms/Title.vue
@@ -0,0 +1,39 @@
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+
+@Component({
+  name: "StartViewTitle",
+})
+export default class StartViewTitle extends Vue {
+  public className = "StartView__Title";
+}
+</script>
+
+<template>
+  <header :class="className">
+    <strong class="bold">
+      <slot name="bold" />
+    </strong>
+    <span class="normal">
+      <slot name="normal" />
+    </span>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+.StartView__Title {
+  font-size: 1.5rem;
+  text-align: center;
+  display: flex;
+  flex-flow: row wrap;
+
+  .bold,
+  .normal {
+    width: 100%;
+  }
+
+  .bold {
+    font-weight: 700;
+  }
+}
+</style>
