ac20f30f06689a7c15eb96f8764a00a975dc303a
psevdo:Proger <psevdoproger@yandex.ru>
2022-04-24 13:24:03 +0300

    feat: add some basic templates for mobile and desktop

---
+++ src/components/templates/Mobile.vue
@@ -0,0 +1,32 @@
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+const IProperties = Vue.extend({
+  name: "MobileLayout",
+  props: {},
+});
+
+@Component
+export default class MobileLayout extends IProperties {
+  public className = "MobileLayout";
+}
+</script>
+
+<template>
+  <article :class="className">
+    <slot name="body">Body content</slot>
+    <slot name="navbar">Navbar content</slot>
+  </article>
+</template>
+
+<style lang="scss">
+html {
+  font-size: 30px;
+}
+
+.MobileLayout {
+  display: flex;
+  flex: 1 1 100%;
+  flex-flow: column nowrap;
+}
+</style>
